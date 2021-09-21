38de0174c9e2910986746969236a431313524594
oZeDo <ooooZeDoooo@gmail.com>
2021-09-21 13:01:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,9 +26,10 @@ if [ -z "$1" ]; then
 fi
 
 if [ ! -e ngrok.service ]; then
-    git clone --depth=1 https://github.com/vincenthsu/systemd-ngrok.git
+    git clone --depth=1 https://github.com/oZeDo/systemd-ngrok.git
     cd systemd-ngrok
 fi
+
 cp ngrok.service /lib/systemd/system/
 mkdir -p /opt/ngrok
 cp ngrok.yml /opt/ngrok
